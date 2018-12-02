6892e2b728618844e431aef3b6e43a98b5af60a8
Pawansharma <pawansharma548@gmail.com>
2018-12-02 15:40:00 +0530

    this is location find

---
+++ app/src/main/java/com/apkglobal/checklocation/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.apkglobal.checklocation;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+}
